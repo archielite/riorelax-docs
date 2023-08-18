06b8de481106f74dfb2868a9137350f19846dc03
Bui Le Anh Nguyen <nguyen@archielite.com>
2023-08-18 18:07:59 +0700

    set up riorelax docs

---
+++ sidebar.ts
@@ -0,0 +1,40 @@
+import { DefaultTheme } from 'vitepress'
+
+export default [
+  { text: 'Overview', link: '/riorelax/' },
+  {
+    text: 'Prologue',
+    items: [
+      { text: 'Release Notes', link: '/riorelax/releases' },
+      { text: 'Upgrade Guide', link: '/riorelax/upgrade' },
+    ]
+  },
+  {
+    text: 'Getting Started',
+    items: [
+      { text: 'Installation', link: '/riorelax/installation' },
+      { text: 'SSL', link: '/riorelax/ssl' },
+      { text: 'License', link: '/riorelax/license' },
+    ],
+  },
+  {
+    text: 'Usage',
+    items: [
+      { text: 'Setup homepage',  link: '/riorelax/usage-homepage' },
+      { text: 'Setup menus',  link: '/riorelax/usage-menus' },
+      { text: 'Setup theme options',  link: '/riorelax/usage-theme-options' },
+      { text: 'Widgets', link: '/riorelax/usage-widgets' },
+      { text: 'Custom CSS/JS', link: '/riorelax/usage-custom-css-js' },
+      { text: 'Translation', link: '/riorelax/usage-translation' },
+      { text: 'Multi-language', link: '/riorelax/usage-multi-language' },
+      { text: 'Currencies', link: '/riorelax/usage-currencies' },
+      { text: 'Location', link: '/riorelax/usage-location' },
+      { text: 'Analytics', link: '/riorelax/usage-analytics' },
+      { text: 'Media - Setup Amazon S3', link: '/riorelax/usage-media-s3' },
+      { text: 'Media - Setup BunnyCDN', link: '/riorelax/usage-media-bunnycdn' },
+      { text: 'Media - Setup Wasabi', link: '/riorelax/usage-media-wasabi' },
+      { text: 'Setup email', link: '/riorelax/usage-email' },
+      { text: 'Backup', link: '/riorelax/plugin-backup' },
+    ],
+  },
+] satisfies DefaultTheme.SidebarItem[]
